22ee4c00633e36fdb00a9c26552a13614d3e9210
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-01 14:24:51 +0530

    new

---
+++ first.c
@@ -0,0 +1,21 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("hero world!\n");
+    printf("aditya world!\n");
+
+    return 0;
+}
+
+
+
