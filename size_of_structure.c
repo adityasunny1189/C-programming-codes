03263d36d4bca2396d4ce2379a3ec0fcca747f8c
adityasunny1189 <adityapathak1189@gmail.com>
2019-08-02 23:46:36 +0530

    new commit

--- size_of_structure.c
+++ size_of_structure.c
@@ -12,3 +12,27 @@ int main()
     printf("size of structure variable is : %d",x);
     return 0;
 }
+
+/*  Memory are like boxes each box constitute one byte means 8 bit 
+    in 32-bit system 4 bytes constitue one 32 bit
+    whenever memory is allocated it is allocated in the form of
+    32 bit means 4 byte are allocated 
+
+    here above 10 char + 12 char + 1 int 
+    means 10 byte + 12 byte + 4 byte = 26 byte should be the size 
+    but actually the size is 28 byte because in 32 bit system 4 bytes are 
+    allocated at a time so for 24 bytes it is allocated but for rest 2 bytes
+    4 byte of space is allocated so total size becomes 28 byte
+    that additional 2 byte of memory is of no use and is just the wastage of memory
+        ________
+       |_\_\_\_\|   }8 bit
+       |_\_\_\_\|   }8 bit ---\  32
+       |________|   }8 bit ---/  bit
+       |________|   }8 bit
+       |________|
+       |________|
+       |________|
+       |________|
+
+
+ */
