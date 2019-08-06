bf55127911045e63bf7810a97eba0e91cdf5090e
adityasunny1189 <adityapathak1189@gmail.com>
2019-08-06 21:52:09 +0530

    new commit

--- multiplication_table.c
+++ multiplication_table.c
@@ -6,5 +6,15 @@ int main()
     scanf("%d",&i);
     for(int j = 1; j < 11; j++)
         printf("%d X %d = %d\n",i,j,i*j);
+    
+    printf("multiplication table from 15 to 20 is as follows\n");
+    for(int k = 1; k < 11; k++)
+    {
+        for(int l = 15; l < 21; l++)
+        {
+            printf("%d X %d = %d\t",l,k,l*k);
+        }
+        printf("\n");
+    }
     return 0;
 }
