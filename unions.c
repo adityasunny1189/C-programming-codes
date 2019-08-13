313856cb1ab13f23dfe638aa4adc4665f207368a
adityasunny1189 <adityapathak1189@gmail.com>
2019-08-13 21:29:39 +0530

    new commit

--- unions.c
+++ unions.c
@@ -4,12 +4,15 @@ int main()
     union num
     {
         int no;
-        char a[4];
+        // char a[4];
+        char b;
     };
     union num N1;
-    N1.no = 512;
-    for(int i= 0 ; i<4; i++)
-        printf("%d",N1.a[i]);
+    N1.b = 'B';
+    printf("%d",N1.no);
+    // N1.no = 512;
+    // for(int i= 0 ; i<4; i++)
+    //     printf("%d\n",N1.a[i]);
     printf("\n");
     return 0;
 }
