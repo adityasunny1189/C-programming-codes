de01c1dc18f55d84984406aba200d3798ae30eae
adityasunny1189 <adityapathak1189@gmail.com>
2019-08-23 22:38:44 +0530

    new commit

--- date_one.c
+++ date_one.c
@@ -7,7 +7,25 @@ int main()
         unsigned int month:6;
         unsigned int year;
     }D1 = {8,8,2019};
-    // printf("enter date\n");
     printf("entered date is %d-%d-%d",D1.day,D1.month,D1.year);
+    unsigned int month_arr[12] = [31,28,31,30,31,30,31,31,30,31,30,31];
+    int days = 45;
+    switch(D1.month)
+    {
+        case 2:
+                if(D1.year % 4 == 0)
+                {
+                    printf("New Date is %d-%d-%d",);
+                }
+                else
+                {
+                    printf("New Date is %d-%d-%d",);
+                }
+                break;
+        case 1:
+        case 3:
+        case 5:
+
+    }
     return 0;
 }
